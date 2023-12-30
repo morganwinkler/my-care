c898f783428c6eeb7147cd9a9270abb890af1fa1
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-12-30 12:09:54 -0500

    Add user model/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create!([
+  { name: "Mike", email: "mike@gmail.com", password: "password", password_confirmation: "password" },
+  { name: "Morgan", email: "morgan@gmail.com", password: "password", password_confirmation: "password" },
+  { name: "Tracy", email: "tracy@gmail.com", password: "password", password_confirmation: "password" },
+  { name: "Scott", email: "scott@gmail.com", password: "password", password_confirmation: "password" },
+])
