f0842b11281b071a23205eeb7bbae62b90caa22c
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-12-30 13:29:07 -0500

    Add doctor model/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,19 @@
-User.create!([
-  { name: "Mike", email: "mike@gmail.com", password: "password", password_confirmation: "password" },
-  { name: "Morgan", email: "morgan@gmail.com", password: "password", password_confirmation: "password" },
-  { name: "Tracy", email: "tracy@gmail.com", password: "password", password_confirmation: "password" },
-  { name: "Scott", email: "scott@gmail.com", password: "password", password_confirmation: "password" },
-])
+# User.create!([
+#   { name: "Mike", email: "mike@gmail.com", password: "password", password_confirmation: "password" },
+#   { name: "Morgan", email: "morgan@gmail.com", password: "password", password_confirmation: "password" },
+#   { name: "Tracy", email: "tracy@gmail.com", password: "password", password_confirmation: "password" },
+#   { name: "Scott", email: "scott@gmail.com", password: "password", password_confirmation: "password" },
+# ])
 
-Visit.create!([
-  { start_date: Date.new(2023, 12, 01), hospital: "Morton Plant", reason: "broken leg", user_id: 1 },
-  { start_date: Date.new(2023, 8, 15), hospital: "Morton Plant", reason: "heartburn", user_id: 1 },
+# Visit.create!([
+#   { start_date: Date.new(2023, 12, 01), hospital: "Morton Plant", reason: "broken leg", user_id: 1 },
+#   { start_date: Date.new(2023, 8, 15), hospital: "Morton Plant", reason: "heartburn", user_id: 1 },
+# ])
+
+Doctor.create!([
+  { name: "Rob Smith", specialty: "Orthopedic", visit_id: 1, note: "blah blah" },
+  { name: "Karen Hodge", specialty: "Surgeon", visit_id: 1, note: "blah blah" },
+  { name: "Lisa Smith", specialty: "General", visit_id: 1, note: "blah blah" },
+  { name: "Bob Bob", specialty: "Cardio", visit_id: 2, note: "blah blah" },
+  { name: "Lisa Bob", specialty: "Gastro", visit_id: 2, note: "blah blah" },
 ])
